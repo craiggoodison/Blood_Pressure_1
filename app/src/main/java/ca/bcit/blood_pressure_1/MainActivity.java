fae660df68c825af1743f045a06465d835681dcb
Daniel Chu <danieljanchu@outlook.com>
2019-10-30 16:24:51 -0700

    updated setCondition

--- app/src/main/java/ca/bcit/blood_pressure_1/MainActivity.java
+++ app/src/main/java/ca/bcit/blood_pressure_1/MainActivity.java
@@ -16,8 +16,15 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String NORMAL = "Normal";
+    private final String ELEVATED = "Elevated";
+    private final String HIGH1 = "High Blood Pressure (Stage 1)";
+    private final String HIGH2 = "High Blood Pressure (Stage 2)";
+    private final String HYPERTENSIVECRISIS = "Hypertensive Crisis";
+
     EditText systolicReading;
     EditText diastolicReading;
+    TextView conditionValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
 //        Date currTime = Calendar.getInstance().getTime();
         String currTime = new SimpleDateFormat(
-                "HH:mm:ss", Locale.getDefault()).format(new Date());
+                "HH:mm", Locale.getDefault()).format(new Date());
         String currDate = new SimpleDateFormat(
                 "dd-MM-yyyy", Locale.getDefault()).format(new Date());
 
@@ -38,12 +45,25 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void setCondition() {
+    public void setCondition(View view) {
+        conditionValue = findViewById(R.id.tvCondition);
         systolicReading = findViewById(R.id.etSystolicReading);
         diastolicReading = findViewById(R.id.etDiastolicReading);
         int systolic = Integer.parseInt(systolicReading.getText().toString());
         int diastolic = Integer.parseInt(diastolicReading.getText().toString());
 
+        // logic not fully working...
+        if (systolic < 120 && diastolic < 80) {
+            conditionValue.setText(NORMAL);
+        } else if ((systolic >= 120 && systolic <= 129) && diastolic < 80) {
+            conditionValue.setText(ELEVATED);
+        } else if ((systolic >= 130 && systolic <= 139) || (diastolic >= 80 && diastolic <= 89)) {
+            conditionValue.setText(HIGH1);
+        } else if ((systolic > 140 && systolic < 180) || (diastolic > 90 && diastolic < 120)) {
+            conditionValue.setText(HIGH2);
+        } else {
+            conditionValue.setText(HYPERTENSIVECRISIS);
+        }
 
     }
 
