8dd922637efd86c6326762ca1fd8c19562ecb97a
craiggoodison <cgoodison1@my.bcit.ca>
2019-11-05 19:43:48 -0800

    Added update method and updat dialog method ,one error

--- app/src/main/java/ca/bcit/blood_pressure_1/MainActivity.java
+++ app/src/main/java/ca/bcit/blood_pressure_1/MainActivity.java
@@ -1,6 +1,7 @@
 package ca.bcit.blood_pressure_1;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.sql.Time;
@@ -13,7 +14,9 @@ import java.util.TimeZone;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -190,5 +193,93 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, History.class);
         startActivity(intent);
     }
+    private void updatePatient(String patientId, int systolicReading, int diastolicReading,
+                               String condition, String readingTime, String readingDate ) {
 
+        DatabaseReference dbRef = databaseBloodPressure.child(patientId);
+
+        Patient patient = new Patient(patientId, systolicReading, diastolicReading, condition,
+                readingTime, readingDate);
+
+        Task setValueTask = dbRef.setValue(patient);
+
+        setValueTask.addOnSuccessListener(new OnSuccessListener() {
+            @Override
+            public void onSuccess(Object o) {
+                Toast.makeText(MainActivity.this,
+                        "Patient Updated.",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        setValueTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this,
+                        "Something went wrong.\n" + e.toString(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    private void showUpdateDialog(final String patientId, int systolicReading, int diastolicReading,
+                                  String condition, final String readingDate, final String readingTime) {
+
+
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+
+        LayoutInflater inflater = getLayoutInflater();
+
+        final View dialogView = inflater.inflate(R.layout.update_dialog, null);
+        dialogBuilder.setView(dialogView);
+
+        final EditText etUserID = dialogView.findViewById(R.id.etUserID);
+        etUserID.setText(patientId);
+
+        final EditText etSystolicReading = dialogView.findViewById(R.id.etSystolicReading);
+        etSystolicReading.setText(systolicReading);
+
+        final EditText etDiastolicReading = dialogView.findViewById(R.id.etSystolicReading);
+        etDiastolicReading.setText(diastolicReading);
+
+        final EditText etCondition = dialogView.findViewById(R.id.etCondition);
+        etCondition.setText(diastolicReading);
+
+        final EditText etReadingDate  = dialogView.findViewById(R.id.etReadingDate);
+        etReadingDate.setText(diastolicReading);
+
+        final EditText etReadingTime  = dialogView.findViewById(R.id.etReadingTime);
+        etReadingTime.setText(diastolicReading);
+
+        final Button btnUpdate = dialogView.findViewById(R.id.btnUpdate);
+        dialogBuilder.setTitle("Update Patient " + patientId + " " + systolicReading + " " + diastolicReading);
+
+        final AlertDialog alertDialog = dialogBuilder.create();
+        alertDialog.show();
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String patientId = etUserID.getText().toString().trim();
+                String systolicReading = etSystolicReading.getText().toString().trim();
+                String diastolicReading = etDiastolicReading.getText().toString().trim();
+                String condition = etCondition.getText().toString().trim();
+
+                if (TextUtils.isEmpty(patientId)) {
+                    etUserID.setError("ID is required");
+                    return;
+                } else if (TextUtils.isEmpty(systolicReading)) {
+                    etSystolicReading.setError("Systolic Reading is required");
+                    return;
+                } else if (TextUtils.isEmpty(systolicReading)) {
+                    etDiastolicReading.setError("Diastolic Reading is required");
+                    return;
+                }
+
+                updatePatient(patientId, systolicReading, diastolicReading, condition,
+                        readingTime, readingDate);
+
+                alertDialog.dismiss();
+            }
+        });
+    }
 }
+
