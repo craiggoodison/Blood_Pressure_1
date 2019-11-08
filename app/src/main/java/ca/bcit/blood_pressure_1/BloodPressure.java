ab0c96cf169869b94dc98b72bbfdaf7b456f99c8
Daniel Chu <danieljanchu@outlook.com>
2019-11-07 17:55:27 -0800

    added BloodPressure.java to extract data from db

---
+++ app/src/main/java/ca/bcit/blood_pressure_1/BloodPressure.java
@@ -0,0 +1,46 @@
+package ca.bcit.blood_pressure_1;
+
+public class BloodPressure {
+
+    private int diastolicReading;
+
+    private String patientId;
+
+    private String readingDate;
+
+    private String readingTime;
+
+    private int systolicReading;
+
+    public void setDiastolicReading(int diastolicReading){
+        this.diastolicReading = diastolicReading;
+    }
+    public int getDiastolicReading(){
+        return this.diastolicReading;
+    }
+    public void setPatientId(String patientId){
+        this.patientId = patientId;
+    }
+    public String getPatientId(){
+        return this.patientId;
+    }
+    public void setReadingDate(String readingDate){
+        this.readingDate = readingDate;
+    }
+    public String getReadingDate(){
+        return this.readingDate;
+    }
+    public void setReadingTime(String readingTime){
+        this.readingTime = readingTime;
+    }
+    public String getReadingTime(){
+        return this.readingTime;
+    }
+    public void setSystolicReading(int systolicReading){
+        this.systolicReading = systolicReading;
+    }
+    public int getSystolicReading(){
+        return this.systolicReading;
+    }
+
+}
